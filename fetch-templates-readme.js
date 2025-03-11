c4a1ea802aa9a6e1f19eb5d87acf838fe1a469e4
KatieHarris2397 <138803843+KatieHarris2397@users.noreply.github.com>
2025-03-11 14:14:42 -0400

    Update fetch-templates-readme.js

--- fetch-templates-readme.js
+++ fetch-templates-readme.js
@@ -1,24 +1,14 @@
 const url = 'https://raw.githubusercontent.com/Nexlayer/templates/main/README.md';
 
-
 async function fetchAndConvert() {
   try {
     const response = await fetch(url);
-    const markdown = await response.text();
-
-    const frontmatter = `---
-    title: Nexlayer Yaml
-    description: 
-    icon: 'file-lines'
-    ---
-    
-    `;
-  
-      const finalMDX = frontmatter + markdown;
-  
+    const markdown = await response.text(); 
   } catch (error) {
       console.error('Process failed:', error);
   }
+
+  return markdown;
 }
 
-fetchAndConvert();
+//fetchAndConvert();
